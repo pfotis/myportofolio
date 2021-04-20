88a68a8a8a260217e26d7563ac25e9819899853a
pfotis <69961508+pfotis@users.noreply.github.com>
2021-04-20 20:18:10 +1000

    Updates

---
+++ precache-manifest.e9f4d5e128ded0ee9a0b54ec9761ec92.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef3406e61a37863a437d76c7758799ec",
+    "url": "/portofolio/index.html"
+  },
+  {
+    "revision": "a3201d6307e065b2eb1b",
+    "url": "/portofolio/static/css/main.fa273bdf.chunk.css"
+  },
+  {
+    "revision": "9944eef849f0d533a63b",
+    "url": "/portofolio/static/js/2.4e50959b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portofolio/static/js/2.4e50959b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3201d6307e065b2eb1b",
+    "url": "/portofolio/static/js/main.4ac96c0c.chunk.js"
+  },
+  {
+    "revision": "c4b75db0e369f311c154",
+    "url": "/portofolio/static/js/runtime-main.5e13a73c.js"
+  },
+  {
+    "revision": "3d59fa6ad94fd4700252df9694684af8",
+    "url": "/portofolio/static/media/Resume.3d59fa6a.pdf"
+  },
+  {
+    "revision": "3e7198d1071d07d65cdc7da0cd808ba5",
+    "url": "/portofolio/static/media/aboutMe.3e7198d1.png"
+  },
+  {
+    "revision": "e6d4d4c456be4d06c70418dedf4306dc",
+    "url": "/portofolio/static/media/add_cities.e6d4d4c4.png"
+  },
+  {
+    "revision": "4f3db17ab828a748d20e9031f4d700d3",
+    "url": "/portofolio/static/media/cssFile1.4f3db17a.png"
+  },
+  {
+    "revision": "c781cc50aa0798e6a70399654d647124",
+    "url": "/portofolio/static/media/employeeList.c781cc50.png"
+  },
+  {
+    "revision": "4671ca5b0494aa9b04a305b08741ea48",
+    "url": "/portofolio/static/media/first_impress.4671ca5b.png"
+  },
+  {
+    "revision": "ee1818be5dbbe548faba948cdd28123a",
+    "url": "/portofolio/static/media/first_impressd.ee1818be.png"
+  },
+  {
+    "revision": "460e4cea0f16c850844c519924d7a7c6",
+    "url": "/portofolio/static/media/first_look.460e4cea.png"
+  },
+  {
+    "revision": "33a1b5fb1eda1b482bac80c6cd2eb096",
+    "url": "/portofolio/static/media/in_the_begin.33a1b5fb.png"
+  },
+  {
+    "revision": "919f12c6ddd1f0ee08e8f29837c3c2d1",
+    "url": "/portofolio/static/media/index.919f12c6.png"
+  },
+  {
+    "revision": "4cb74b61200caba41c430c531a93dd98",
+    "url": "/portofolio/static/media/me.4cb74b61.JPG"
+  },
+  {
+    "revision": "ff9868ad75aef23101dd51643aa713d3",
+    "url": "/portofolio/static/media/readme1.ff9868ad.png"
+  },
+  {
+    "revision": "47419ac044284910405518327c87b22f",
+    "url": "/portofolio/static/media/startQuiz.47419ac0.png"
+  },
+  {
+    "revision": "ccdc1d229359a72bffeb4bc09137b68c",
+    "url": "/portofolio/static/media/teamPhoto.ccdc1d22.png"
+  }
+]);

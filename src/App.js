fd4baf52bed411eb069f8b7a35f22de515001b95
pfotis <69961508+pfotis@users.noreply.github.com>
2021-04-20 12:11:09 +1000

    create footer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import About from "./pages/About";
-// import Contact from "./pages/Contact";
-// import Portofolio from "./pages/Portofolio";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Wrapper from "./components/Wrapper";
+
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Wrapper>
-          <Route exact path="/" component={About} />
-          <Route exact path="/about" component={About} />
-          {/* <Route exact path="/discover" component={Contact} />
-          <Route exact path="/search" component={Portofolio} /> */}
-        </Wrapper>
-        <Footer />
-      </div>
-    </Router>
+    
   );
 }
 
